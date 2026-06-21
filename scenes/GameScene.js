a5bd11930fb105c1a7c159e289d3eb58db1c0d95
ｵｶｵ <aotakokeshi@gmail.com>
2026-06-21 11:42:10 +0900

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -6,10 +6,23 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
 
+        // 地面の位置
+        const groundY = 650;
+
+        // 地面を長方形で描画
+        // x, y, 幅, 高さ, 色
+        this.add.rectangle(
+            640,
+            groundY,
+            1280,
+            140,
+            0x228B22
+        );
+
         // 仮の文字
         this.add.text(
             500,
-            300,
+            100,
             "Game Scene",
             {
                 fontSize: "48px",
